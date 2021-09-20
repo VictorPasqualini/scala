43aae977138ef12303d3b5a863f36bd68fd54530
Victor Pasqualini <victor.pasqualini@outlook.com>
2021-09-20 09:53:58 -0300

    Create day5_loops.scala

---
+++ HackerRank/day5_loops.scala
@@ -0,0 +1,28 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
+object Solution {
+    def main(args: Array[String]) {
+        val n = StdIn.readLine.trim.toInt
+        
+        for(i <- 1 to 10) {
+            println(n + " x " + i + " = " + (n * i))
+        }
+    }
+}
